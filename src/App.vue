8b8df8cb30dc1e0306666997397ba46defff1e66
mtbotherd <mtbotherd@users.noreply.github.com>
2019-03-25 15:50:34 -0500

    Added Proxima Nova font.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-	<div id="app">
-		<header>
-			<NavBar />
-		</header>
-		<div class="container">
-			<router-view />
-		</div>
-	</div>
+  <div id="app">
+    <header>
+      <NavBar/>
+    </header>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,6 +19,9 @@ export default {
 	}
 }
 </script>
+<style>
+@import 'https://use.typekit.net/tev0qmq.css';
+</style>
 <style lang="scss">
 @import './assets/scss/app.scss';
 </style>
