e6eaa772518744095faf23312b01b222c8f954c7
Rich Bothe <rich.bothe@metrotransit.org>
2019-04-09 00:45:27 -0500

    Set active/inactive icons for tabs

--- src/components/TriptoolsTabs.vue
+++ src/components/TriptoolsTabs.vue
@@ -2,13 +2,23 @@
 	<div id="tripToolsTabs">
 		<b-card no-body>
 			<b-tabs>
-				<b-tab title-item-class="mr-2" title-link-class="p-md-3" active>
+				<b-tab
+					title-item-class="trip-planner-tab mr-2"
+					title-link-class="p-md-3"
+					active
+				>
 					<template slot="title">
 						<IconSprite
 							name="icon-lg-directions-color"
 							width="37"
 							height="32"
-							class="d-block d-md-inline-block mx-auto"
+							class="svg-active d-block d-md-inline-block mx-auto"
+						/>
+						<IconSprite
+							name="icon-lg-directions-white"
+							width="37"
+							height="32"
+							class="svg-inactive d-block d-md-inline-block mx-auto"
 						/>
 						Trip Planner
 					</template>
@@ -17,10 +27,16 @@
 				<b-tab title-item-class="mr-2" title-link-class="p-md-3">
 					<template slot="title">
 						<IconSprite
+							name="icon-lg-clock-color"
+							width="32"
+							height="32"
+							class="svg-active d-block d-md-inline-block ml-auto mr-auto"
+						/>
+						<IconSprite
 							name="icon-lg-clock-white"
 							width="32"
 							height="32"
-							class="d-block d-md-inline-block ml-auto mr-auto"
+							class="svg-inactive d-block d-md-inline-block ml-auto mr-auto"
 						/>
 						NexTrip
 					</template>
@@ -29,10 +45,16 @@
 				<b-tab title-link-class="p-md-3">
 					<template slot="title">
 						<IconSprite
+							name="icon-lg-triangle-exclamation-color"
+							width="37"
+							height="32"
+							class="svg-active d-block d-md-inline-block ml-auto mr-auto"
+						/>
+						<IconSprite
 							name="icon-lg-triangle-exclamation-white"
 							width="37"
 							height="32"
-							class="d-block d-md-inline-block ml-auto mr-auto"
+							class="svg-inactive d-block d-md-inline-block ml-auto mr-auto"
 						/>
 						Alerts
 					</template>
@@ -40,7 +62,32 @@
 				</b-tab>
 			</b-tabs>
 		</b-card>
-		<!-- <ul
+	</div>
+</template>
+
+<script>
+// import JQuery from 'jquery'
+// let $ = JQuery
+
+// $('.nav-tabs li').click(function() {
+// 	$(this).toggleClass('strike')
+// 	var icon = 'icon-'
+// 	icon += !$(this).hasClass('strike')
+// 		? 'icon-lg-directions-white'
+// 		: 'icon-lg-directions-color'
+
+// 	$('svg', this).attr('class', icon)
+// 	$('svg use', this).attr('xlink:href', '#' + icon)
+// })
+
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
+
+<!-- 
+// Normal Bootstrap markup. 
+    <ul
 			id="tripTools"
 			role="tablist"
 			class="nav nav-tabs nav-justified d-flex align-items-end"
@@ -101,11 +148,3 @@
 				</a>
 			</li>
     </ul>-->
-	</div>
-</template>
-
-<script>
-export default {}
-</script>
-
-<style lang="scss" scoped></style>
