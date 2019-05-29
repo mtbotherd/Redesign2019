5acea5ec77d7c95fafd18b59a0ba966161bbae6d
Rich Bothe <rich.bothe@metrotransit.org>
2019-05-29 16:49:31 -0500

    NexTrip - Added initial fields.

--- src/components/NexTrip/NexTripModule.vue
+++ src/components/NexTrip/NexTripModule.vue
@@ -1,13 +1,50 @@
 <template>
     <div id="nexTripModule">
         <h1 class="sr-only">NexTrip</h1>
-        <p>The NexTrip module will live here.</p>
+        <h2 class="text-center">Real-time Departures</h2>
+        <div class="row">
+            <div class="col-md-5">
+                <div class="form-group">
+                    <label for="selectRoute" class="sr-only">Select Route</label>
+                    <select
+                        id="selectRoute"
+                        name="selectRoute"
+                        class="custom-select custom-select-lg"
+                    >
+                        <option value="route-1">Route 1</option>
+                        <option value="route-2">Route 2</option>
+                        <option value="route-2">Route 3</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <p class="h4 text-center">or</p>
+            </div>
+            <div class="col-md-5">
+                <div class="form-group">
+                    <input
+                        id="stopNumber"
+                        v-model="stopNumber"
+                        type="text"
+                        name="stopNumber"
+                        class="form-control form-control-lg"
+                        required
+                    >
+                    <label for="stopNumber" class="form-control-placeholder">Enter Stop Number</label>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-	name: 'NexTripModule'
+	name: 'NexTripModule',
+	data() {
+		return {
+			stopNumber: ''
+		}
+	}
 }
 </script>
 
