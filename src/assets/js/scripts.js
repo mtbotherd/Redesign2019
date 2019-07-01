331d63d37d39703b671853944bb5a156ce87c9e9
Rich Bothe <rich.bothe@metrotransit.org>
2019-07-01 11:26:53 -0500

    Replacing Vue/webpack with gulp/jquery

--- src/assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-// Trip Planner
-var inputs = $('.tp-from-location, .tp-to-location'),
-	tmp
-$('.icon-sm-switch-blue').click(function() {
-	tmp = inputs[0].value
-	inputs[0].value = inputs[1].value
-	inputs[1].value = tmp
-})
