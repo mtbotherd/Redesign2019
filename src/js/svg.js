1498f79a7c0b25852495a884c044d5faf3b69eff
Rich Bothe <rich.bothe@metrotransit.org>
2019-07-10 14:50:17 -0500

    Menu icon functionality

--- src/js/svg.js
+++ src/js/svg.js
@@ -1,10 +1,24 @@
-$('svg.hover, svg.active').hide();
+$('svg.active').hide();
 
-$('#header .dropdown').hover(function() {
-	$(this).find('svg.hover').show();
+var dropdown = $('#header .dropdown');
+$(dropdown).hover(function() {
+	$(this).find('svg.active').show();
 	$(this).find('svg.normal').hide();
 }, function() {
 	$(this).find('svg.normal').show();
-	$(this).find('svg.hover').hide();
+	$(this).find('svg.active').hide();
 }
 );
+
+// var rotated = false;
+// $(dropdown).click(function() {
+// 	if (!rotated) {
+// 	$(this).find('.dropdown-toggle').addClass('active');
+// 	$(this).find("svg.active").css({ "transform": "rotate(180deg)" });
+// 	} else {
+// 	$(this).find('.dropdown-toggle').removeClass('active');
+// 	$(this).find("svg.normal").css({ "transform": "rotate(0deg)" });
+// 	}
+// 	// Toggle the flag
+// 	//rotated = !rotated;
+// });
