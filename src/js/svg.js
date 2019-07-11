7548883fbbe6eea92130d52b6ccbffa7c0f8bc9f
Rich Bothe <rich.bothe@metrotransit.org>
2019-07-11 00:02:45 -0500

    Converting to Gulp progress.

--- src/js/svg.js
+++ src/js/svg.js
@@ -1,24 +1,17 @@
+/***********************************************
+			SVG icon interactions
+***********************************************/
+// Header menu
 $('svg.active').hide();
 
-var dropdown = $('#header .dropdown');
-$(dropdown).hover(function() {
+var menu = $('#header .nav-item');
+$(menu).hover(function() {
 	$(this).find('svg.active').show();
-	$(this).find('svg.normal').hide();
+	$(this).find('svg.inactive').hide();
 }, function() {
-	$(this).find('svg.normal').show();
+	$(this).find('svg.inactive').show();
 	$(this).find('svg.active').hide();
 }
 );
 
-// var rotated = false;
-// $(dropdown).click(function() {
-// 	if (!rotated) {
-// 	$(this).find('.dropdown-toggle').addClass('active');
-// 	$(this).find("svg.active").css({ "transform": "rotate(180deg)" });
-// 	} else {
-// 	$(this).find('.dropdown-toggle').removeClass('active');
-// 	$(this).find("svg.normal").css({ "transform": "rotate(0deg)" });
-// 	}
-// 	// Toggle the flag
-// 	//rotated = !rotated;
-// });
+// Trip Tools Tabs
