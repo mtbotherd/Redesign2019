46c0ee2e704c3b03d6c621a7e11cae25a188afb0
Rich Bothe <rich.bothe@metrotransit.org>
2019-08-02 14:44:56 -0500

    json and ajax example.

--- src/js/json-test.js
+++ src/js/json-test.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+    // This variable is used in the ajax url request to increase the page count by 1 to get additional json data.
+    var pageCounter = 1;
+
     // HTML element that will hold the data.
     var animalContainer = $('#animalInfo');
 
@@ -12,7 +15,7 @@ $(document).ready(function() {
         var ourRequest = new XMLHttpRequest(); // Expects the ".open" method.
 
         // Use the "open" method to tell the browser to "get" the data (GET is the first argument) from the url provided in the second argument.
-        ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+        ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
 
         // Use the "onload" method to say what we want to happen
         ourRequest.onload = function() {
@@ -31,18 +34,47 @@ $(document).ready(function() {
 
         // Send request
         ourRequest.send();
+
+        // Increment page counter by 1.
+        pageCounter++;
+
+        if (pageCounter > 3) {
+            btn.hide();
+        }
     });
 
     // A new function to create and and html to the page.
     function renderHTML(data) { // Include 1 parameter.
 
         // Empty string to concatenate our data to.
-        htmlString = "";
+        htmlString = '';
 
         // Loop through objects
         for (i = 0; i < data.length; i++) {
             // concatenate data to htmlStrong
-            htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
+            htmlString += '<p>' + data[i].name + ' is a ' + data[i].species + ' that likes to eat ';
+
+            // loop through nested objects
+            for (ii = 0; ii < data[i].foods.likes.length; ii++) {
+                if (ii == 0) {
+                    htmlString += data[i].foods.likes[ii];
+                } else {
+                    htmlString += ' and ' + data[i].foods.likes[ii];
+                }
+            }
+
+            htmlString += ' and dislikes ';
+
+            for (ii = 0; ii < data[i].foods.dislikes.length; ii++) {
+                if (ii == 0) {
+                    htmlString += data[i].foods.dislikes[ii];
+                } else {
+                    htmlString += ' and ' + data[i].foods.dislikes[ii];
+                }
+            }
+
+            // Add on the period and close the paragraph.
+            htmlString += '.</p>';
         }
 
         animalContainer.append(htmlString);
