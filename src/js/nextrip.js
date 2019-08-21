1b3ee35ed69ecd6af613c1186b82fc18c6c88e17
Rich Bothe <rich.bothe@metrotransit.org>
2019-08-20 22:13:19 -0500

    Added validation, Search by stop, other housekeeping.

--- src/js/nextrip.js
+++ src/js/nextrip.js
@@ -152,6 +152,46 @@ var NexTrip = (function ($, window, document, undefined) {
 		});
 
 		$('#searchStopsButton').click(function () {
+			$('#aspnetForm').validate({
+				debug: true,
+				rules: {
+					stopNumber: {
+						required: true,
+						minlength: 4
+					}
+				},
+				messages: {
+					stopNumber: {
+						required: 'X This field needs a stop number',
+						minlength: 'Must be a minimum of four numbers'
+					}
+				},
+				errorElement: 'div',
+		
+				// Replaces default .has-error class with Bootstrap 4 .is-valid class
+				errorClass: 'is-invalid',
+				// Replaces default .has-succes class with Bootstrap 4 .is-valid class
+				validClass: 'is-valid',
+		
+				errorPlacement: function(error, element) {
+					// Add the `help-block` class to the error element
+					error.addClass('help-block');
+		
+					element.parents('.input-group').addClass('has-feedback');
+		
+					$('<span class="form-control-feedback"></span>').insertAfter(element);
+					error.appendTo(element.parents('.input-group'));
+				},
+				success: function(label, element) {},
+				highlight: function(element, errorClass, validClass) {
+					// Adds error '.is-invalid' for Bootstrap 4 styles.
+					$(element).parents('.input-group').addClass(errorClass).removeClass(validClass);
+				},
+				unhighlight: function(element, errorClass, validClass) {
+					// Adds valid class '.is-valid' for Bootstrap 4 styles.
+					$(element).parents('.input-group').addClass(validClass).removeClass(errorClass);
+				}
+			});
 			var stopId = $('#stopNumber').val();
 			if (timer > 0) {
 				clearInterval(timer);
