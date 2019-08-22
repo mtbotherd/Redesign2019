1448c9f59c22c8a29210229c9b4fcd14555020cf
Rich Bothe <rich.bothe@metrotransit.org>
2019-08-22 09:35:17 -0500

    Removed validation due to issue.

--- src/js/validation.js
+++ src/js/validation.js
@@ -88,50 +88,50 @@ $(function () {
 
 	// Trip Planner validation
 	/* to/from location */
-	$('#planMyTrip').click(function () {
-		$('form').validate({
-			debug: true,
-			rules: {
-				fromLocation: {
-					required: true
-				},
-				toLocation: {
-					required: true
-				}
-			},
-			messages: {
-				fromLocation: {
-					required: 'Please select an adsress'
-				},
-				toLocation: {
-					required: 'Please select an address'
-				}
-			},
-			errorElement: 'div',
+	// $('#planMyTrip').click(function () {
+	// 	$('form').validate({
+	// 		debug: true,
+	// 		rules: {
+	// 			fromLocation: {
+	// 				required: true
+	// 			},
+	// 			toLocation: {
+	// 				required: true
+	// 			}
+	// 		},
+	// 		messages: {
+	// 			fromLocation: {
+	// 				required: 'Please select an adsress'
+	// 			},
+	// 			toLocation: {
+	// 				required: 'Please select an address'
+	// 			}
+	// 		},
+	// 		errorElement: 'div',
 	
-			// Replaces default .has-error class with Bootstrap 4 .is-valid class
-			errorClass: 'is-invalid',
-			// Replaces default .has-succes class with Bootstrap 4 .is-valid class
-			validClass: 'is-valid',
+	// 		// Replaces default .has-error class with Bootstrap 4 .is-valid class
+	// 		errorClass: 'is-invalid',
+	// 		// Replaces default .has-succes class with Bootstrap 4 .is-valid class
+	// 		validClass: 'is-valid',
 	
-			errorPlacement: function(error, element) {
-				// Add the `help-block` class to the error element
-				error.addClass('help-block');
+	// 		errorPlacement: function(error, element) {
+	// 			// Add the `help-block` class to the error element
+	// 			error.addClass('help-block');
 	
-				element.parents('.input-group').addClass('has-feedback');
+	// 			element.parents('.input-group').addClass('has-feedback');
 	
-				$('<span class="form-control-feedback"></span>').insertAfter(element);
-				error.appendTo(element.parents('.input-group'));
-			},
-			success: function(label, element) {},
-			highlight: function(element, errorClass, validClass) {
-				// Adds error '.is-invalid' for Bootstrap 4 styles.
-				$(element).parents('.input-group').addClass(errorClass).removeClass(validClass);
-			},
-			unhighlight: function(element, errorClass, validClass) {
-				// Adds valid class '.is-valid' for Bootstrap 4 styles.
-				$(element).parents('.input-group').removeClass(errorClass);
-			}
-		});
-	});
+	// 			$('<span class="form-control-feedback"></span>').insertAfter(element);
+	// 			error.appendTo(element.parents('.input-group'));
+	// 		},
+	// 		success: function(label, element) {},
+	// 		highlight: function(element, errorClass, validClass) {
+	// 			// Adds error '.is-invalid' for Bootstrap 4 styles.
+	// 			$(element).parents('.input-group').addClass(errorClass).removeClass(validClass);
+	// 		},
+	// 		unhighlight: function(element, errorClass, validClass) {
+	// 			// Adds valid class '.is-valid' for Bootstrap 4 styles.
+	// 			$(element).parents('.input-group').removeClass(errorClass);
+	// 		}
+	// 	});
+	// });
 });
