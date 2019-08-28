93d427c2baa060dd33ef46796241c9fdf34d7ce0
JimGonsoski <james.gonsoski@metc.state.mn.us>
2019-08-28 12:51:59 -0500

    update trip plan and add comments

--- src/js/tripplan.js
+++ src/js/tripplan.js
@@ -1,12 +1,21 @@
+//
+// TripPLan encapsulates the call to generate the trip plan
+//
 var TripPlan = function() {
     var TRIPPLANJSON = {};
+    //
+    // newTrip generates a trip from two address locations
+    // 
+    // parameters:
+    // 'fromLocation': the JSON object output from the address autocomplete
+    // 'toLocation': the JSON object output from the address autocomplete
+    // 
     var newTrip = function (fromLocation, toLocation) {
         TRIPPLANJSON = {}; // clear the old one
         console.log("Lets go tripping from " + fromLocation.address + " to " + toLocation.address);
         let testDate = new Date("8/19/2019 07:30:00 AM");
-        let atisID = '0';
-        let fromLoc = fromLocation.address + '|' + fromLocation.location.y + '|' + fromLocation.location.x + '|' + atisID;
-        let toLoc = toLocation.address + '|' + toLocation.location.y + '|' + toLocation.location.x + '|' + atisID;
+        let fromLoc = fromLocation.address + '|' + fromLocation.location.y + '|' + fromLocation.location.x + '|' + fromLocation.ATIS_ID;
+        let toLoc = toLocation.address + '|' + toLocation.location.y + '|' + toLocation.location.x + '|' + toLocation.ATIS_ID;
         $.ajax({
             type: "get",
             url: "https://www.metrotransittest.org/Services/TripPlannerSvc.ashx",
@@ -40,12 +49,25 @@ var TripPlan = function() {
         newTrip: newTrip,
         getTrip: getTrip
     };
-};
+}();
 
+// This triggers planning a new trip when the 
+// button on the Trip Planner page is clicked
+//
 $('button[name="planMyTrip"]').click(function(){
     TripPlan.newTrip(TRIPFROMLOCATION,TRIPTOLOCATION);
 });
 
+// This loads the map into the resulting Trip Plan page.
+// Once the map loads, trips can be displayed.
+//
+// TODO: this is currently in DEMO mode. 
+// Need to add routines that trigger when user
+// clicks the 'trip summary' button to show on the
+// map just that trip option.
+// 
+// Format for the call:
+// TRIM.drawTrip(<trip option>, <entire trip plan>, <zoom to trip>)
 if ($('#tripPlanMap').attr('maptype') === 'trip') {
     TRIM.init('tripPlanMap').then(function() {
         let tripPlan = TripPlan.getTrip();
