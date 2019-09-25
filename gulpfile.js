a46d67acdfa538cc8b4dfcc9ce5318bb9434cb42
Rich Bothe <rich.bothe@metrotransit.org>
2019-09-25 15:44:30 -0500

    Removed .min versions from being copied.

--- gulpfile.js
+++ gulpfile.js
@@ -24,10 +24,10 @@ gulp.task('browserSync', function () {
 // Copy vemdor js to src
 gulp.task('vendorjs', function () {
 	return gulp.src([
-		'node_modules/jquery/dist/jquery.min.js',
-		'node_modules/popper.js/dist/umd/popper.min.js',
-		'node_modules/bootstrap/dist/js/bootstrap.min.js',
-		'node_modules/exlink/jquery.exlink.min.js'
+		'node_modules/jquery/dist/jquery.js',
+		'node_modules/popper.js/dist/umd/popper.js',
+		'node_modules/bootstrap/dist/js/bootstrap.js',
+		'node_modules/exlink/jquery.exlink.js'
 	])
 		.pipe(gulp.dest('src/js'))
 });
