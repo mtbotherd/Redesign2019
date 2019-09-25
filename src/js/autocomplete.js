7757057a42a4d540a258d55ecd2a5840930d0faa
Rich Bothe <rich.bothe@metrotransit.org>
2019-09-25 15:44:52 -0500

    Moved "getUserLocation" to this file.

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -1,6 +1,30 @@
 $(function() {
 	"use strict";
 
+	function getUserLocation() {
+		var location = null;
+		if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+			function(position) {
+			location = {
+				x: position.coords.longitude,
+				y: position.coords.latitude,
+				spatialReference: { wkid: 4326 }
+			};
+			},
+			function(error) {
+			console.warn("getLocation failed: " + error);
+			},
+			{
+			enableHighAccuracy: true,
+			timeout: 5000,
+			maximumAge: 0
+			}
+		);
+		}
+		return location;
+	}
+
 	/***********************************************
 				Begin Autocomplete
 		***********************************************/
