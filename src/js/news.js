01ddbe5970cbc391b46e0796b9bdb1be2073b7b0
JimGonsoski <james.gonsoski@metc.state.mn.us>
2019-09-25 10:44:33 -0500

    Add schedule.js + news.js from mojoportal; update TRIM

---
+++ src/js/news.js
@@ -0,0 +1,30 @@
+﻿var News = (function ($, window, document, undefined) {
+
+    'use strict';
+
+    var init = function () {
+        var newsStories = [];
+        var titles = $('.articleDisplayTitle>a');
+        var links = $('.articleDisplayTitle>a');
+        var imgs = $('.articleDisplayAbstract img');
+
+        for (var i = 0; i < 4; i++) {
+            let title = titles[i] != undefined ? titles[i].text : 'Article';
+            let link = links[i] != undefined ? links[i].href : '#';
+            let img = imgs[i] != undefined ? imgs[i].src : '/img/placeholder-300x300.png';
+
+            newsStories[i] = {
+                title: title,
+                link: link,
+                img: img
+            }
+        }
+
+        //TODO: add news to home page
+    };
+
+    return {
+        init: init
+    };
+
+})(jQuery, window, document);
