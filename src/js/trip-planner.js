a5b5147b3a786fa971c7e9d96d83fe5c44f57ac8
JimGonsoski <james.gonsoski@metc.state.mn.us>
2019-10-07 15:37:00 -0500

    Trip Planner phase 3 add Jon H format code

--- src/js/trip-planner.js
+++ src/js/trip-planner.js
@@ -93,6 +93,206 @@ var TripPlan = (function($, window, document, undefined) {
 	};
 })(jQuery, window, document);
 
+var formatTripPlan = function(plan) {
+	var tripCount = plan.PlannerItin.PlannerOptions.length;
+	var tripMsg = 'We have ' + tripCount.toString() + 'trip';
+	if (tripCount > 1) {
+		tripMsg+= 's';
+	}
+	tripMsg += ' for you.';
+	$("#trips-amount").html(`We have ${plan.PlannerItin.PlannerOptions.length} trips for you.`);
+	//<p class="mb-0">We found 3 trips for you Trips shown are based on your selections and closest departure to 3:25 PM, Friday, August 30th.</p>
+    $(".tp-results").empty();
+    plan.PlannerItin.PlannerOptions.forEach(function(l,i){
+      let list = [],secondList = [];
+      l.Segments.forEach(function(li,ii){
+        switch (li.SegmentType) {
+          case 0:
+            list.push(`<img class="icon"src="/img/svg/bus-gray.svg">&nbsp;
+            <span class="route mr-1">${plan.PlannerItin.PlannerOptions[i].Segments[ii].Route}</span>`)
+            break;
+          case 1:
+            if(li.PublicRoute==="Blue Line"){
+              list.push(`<span class="d-flex align-items-center badge badge-secondary mr-1">
+                <img class="icon icon-lrt-white" src="/img/svg/lrt-white.svg">
+                <span class="caps">Blue</span>
+              </span>`)
+            } else if(li.PublicRoute==="Green Line"){
+              list.push(`<span class="d-flex align-items-center badge badge-success mr-1">
+                <img class="icon icon-lrt-white" src="/img/svg/lrt-white.svg">
+                <span class="caps">Green</span>
+              </span>`)
+            }
+            break;
+          case 2:
+            list.push(`<img class="icon" src="/img/svg/circle-gray-outline-train.svg">`)
+            break;
+          case 3:
+            list.push(`<img class="icon" src="/img/svg/pedestrian-gray.svg">`)
+            break;
+          case 4:
+           list.push(`<img class="icon" src="/img/svg/circle-gray-outline-train.svg">`)
+           break;
+          default:
+            console.log('This animal will not.')
+          }
+      })
+      l.Segments.forEach(function(li,ii){
+        switch(li.SegmentType){
+          case 0:
+            secondList.push(`<div class="leg-item">
+            <div class="d-table-cell leg-time">1:10 p.m.</div>
+            <div class="d-table-cell leg-mode bus">
+              <div class="d-table-cell leg-mode-icon">
+              <img class="icon" src="/img/svg/circle-gray-outline-train.svg">
+              </div>
+              <p>
+                <img class="icon blink"
+                   src="/img/svg/broadcast-red.svg">&nbsp;<strong>Currently 5
+                  <abbr title="minutes">min</abbr> late</strong>
+                <br>
+                Route ${li.Route} ${li.OffStop.StopLocation.LocationName}
+                <br>
+                <a href="/home/#ServiceAlerts">
+                  <small>view alerts</small>
+                </a>
+              </p>
+              <p>
+                <strong>Depart</strong> from ${li.OnStop.StopLocation.LocationName}
+              </p>
+              <p>
+                <strong>Arrive</strong> at ${li.OffStop.StopLocation.LocationName}
+              </p>
+            </div>
+          </div>`)
+            break;
+          case 1:
+            secondList.push(`<div class="leg-item">
+            <div class="d-table-cell leg-time">1:40 p.m.</div>
+            <div class="d-table-cell leg-mode metro-${li.PublicRoute.split(" ", 1)}">
+              <div class="d-table-cell leg-mode-icon">
+                ${li.PublicRoute=== "Blue Line"?'<img class="icon" src="/img/svg/circle-green-outline-lrt.svg"/>':'<img class="icon" src="/img/svg/circle-blue-lrt.svg"/>'}
+              </div>
+              <p>
+                <strong>${li.PublicRoute}</strong> to ${li.Route} ${li.SegmentDestination}
+              </p>
+              <p>
+                <strong>Depart</strong> from ${li.OnStop.StopLocation.LocationName}
+              </p>
+              <p>
+                <strong>Arrive</strong> at ${li.OffStop.StopLocation.LocationName}
+              </p>
+            </div>
+          </div>`)
+            break;
+          case 2:
+            secondList.push(`<div class="leg-item">
+            <div class="d-table-cell leg-time">1:10 p.m.</div>
+            <div class="d-table-cell leg-mode bus">
+              <div class="d-table-cell leg-mode-icon">
+                <img class="icon"
+                   src="/img/svg/circle-gray-outline-bus.svg">
+              </div>
+              <p>
+                <img class="icon blink"
+                   src="/img/svg/broadcast-red.svg">&nbsp;<strong>Currently 5
+                  <abbr title="minutes">min</abbr> late</strong>
+                <br>
+                Route ${li.Route} ${li.SegmentDestination}
+                <br>
+                <a href="/home/#ServiceAlerts">
+                  <small>view alerts</small>
+                </a>
+              </p>
+              <p>
+                <strong>Depart</strong> from ${li.OnStop.StopLocation.LocationName}
+              </p>
+              <p>
+                <strong>Arrive</strong> at ${li.OffStop.StopLocation.LocationName}
+              </p>
+            </div>
+          </div>`)
+            break;
+          case 3:
+            secondList.push(`<div class="leg-item">
+            <div class="d-table-cell leg-time">1:10 p.m.</div>
+            <div class="d-table-cell leg-mode walk">
+              <div class="d-table-cell leg-mode-icon">
+                <img class="icon"
+                   src="/img/svg/circle-green-outline-pedestrian.svg">
+              </div>
+              <p>
+                <strong>Walk</strong>${li.WalkTextOverview}
+                <br>
+                <small>
+                  (about 6
+                  <abbr title="minutes">min</abbr>)
+                </small>
+              </p>
+            </div>
+          </div>`)
+            break;
+          case 4:
+            secondList.push(`<div class="leg-item">
+            <div class="d-table-cell leg-time">1:10 p.m.</div>
+            <div class="d-table-cell leg-mode walk">
+              <div class="d-table-cell leg-mode-icon">
+                <img class="icon"
+                   src="/img/svg/alerts-color.svg">
+              </div>
+              <p>
+                <strong>Walk</strong>${li.WalkTextOverview}
+              </p>
+            </div>
+          </div>`)
+            break;
+          default:
+          console.log(secondList)
+        }
+	  });
+	  
+     
+    $('.tp-results').append(`
+          <div class="card mb-4">
+            <a class="border" data-toggle="collapse" href="#collapse${i}" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapse${i}">
+              <span class="d-flex" role="link">
+                <span class="d-flex align-items-center tp-time">56
+                  min</span>
+                <span class="align-items-center tp-route">`+ list.join('<img class="icon chevron-right-gray mr-2" src="/img/svg/chevron-right-gray.svg">') +`
+                  <img class="icon icon-arrow-right-blue ml-auto" src="/img/svg/arrow-right-blue.svg">
+                </span>
+              </span>
+            </a>
+          </div>
+
+          <div id="collapse${i}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+              <div class="row flex-row">
+                    <div class="col-lg-5">
+                      <div class="d-block tp-results">
+                        `+ secondList.join(" ")+`
+                      </div>
+                      <div class="clearfix"></div>
+                      <hr class="d-block d-lg-none">
+                    </div>
+                    <div class="col-lg-7">
+                      <div class="tp-basemap">
+                        <div class="map-container border">
+                          <div id="tripPlanMap" class="map" mapType="trip" role="application" aria-label="interactive map of transit trip plan">
+                            <div id="trimLocate"></div>
+                            <div class="mapLoading"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+              </div>
+            </div>
+          </div>
+          
+          `);
+    });
+};
+
 $(function () {
     // This triggers planning a new trip when the
     // button on the Trip Planner page is clicked
@@ -103,7 +303,7 @@ $(function () {
     $('button[name="planMyTrip"]').click(function () {
 		$('#tripPlannerResults').hide();
         var tripFromLocation = AutocompleteAddress.getChoice('fromLocation');
-		var userPos = AutocompleteAddress.fetchUserLoc(); // this get the user GPS location, if you need it
+		var userPos = AutocompleteAddress.fetchUserLoc(); // this gets the user GPS location, if you need it
 		var tripToLocation = AutocompleteAddress.getChoice('toLocation');
 		var dateTime = new Date();
 		var selectTimeType = 'Depart';
@@ -133,10 +333,11 @@ $(function () {
             };
             TripPlan.newTrip(tripProperties)
                 .then(function () {
-                    let plan = TripPlan.getTrip();
-                    console.log("Have a Plan");
-					console.dir(plan);
-					if (plan.PlannerItin.PlannerOptions.length > 0) {
+                    let tripPlan = TripPlan.getTrip();
+                    console.log('Have a Plan');
+					console.dir(tripPlan);
+					if (tripPlan.PlannerItin.PlannerOptions.length > 0) {
+						formatTripPlan(tripPlan)
 						$('.trips-found').show();
 						$('.no-trips-found').hide();
 						$('#tripPlannerResults').show();
@@ -147,8 +348,8 @@ $(function () {
 					}
                 })
                 .fail(function (err) {
-                    console.warn("Trip Plan Failed: " + err.Message);
-					console.dir(err);
+                    console.warn('Trip Plan Failed: ' + err.Message);
+					//console.dir(err);
 					$('.trips-found').hide();
 					$('.no-trips-found').show();
 					$('#tripPlannerResults').show();
