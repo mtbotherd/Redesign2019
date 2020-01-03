ce686dde7e5458da326e1174e568387ec51222f3
Rich Bothe <rich.bothe@metrotransit.org>
2020-01-03 12:56:58 -0600

    Latest

--- src/js/park-ride-lots.js
+++ src/js/park-ride-lots.js
@@ -20,61 +20,61 @@
     }
 */
 
-var ParkRideServices = (function($,  window, document, undefined) {
+var ParkRideServices = (function ($, window, document, undefined) {
     // This service requires map coordinates in UTM 
-    function findNearestParkRides (findLoc) {
+    function findNearestParkRides(findLoc) {
         return $.Deferred(function (dfd) {
-            let address = 
-            findLoc.address 
-            + "|"
-            + findLoc.location.y
-            + "|"
-            + findLoc.location.x
-            + "|";
+            let address =
+                findLoc.address
+                + "|"
+                + findLoc.location.y
+                + "|"
+                + findLoc.location.x
+                + "|";
             let fromATIS = '0';
-			if (findLoc.attributes.ATIS_ID.indexOf(';') > -1) {
-			    fromATIS += findLoc.attributes.ATIS_ID.split(';')[1];
+            if (findLoc.attributes.ATIS_ID.indexOf(';') > -1) {
+                fromATIS += findLoc.attributes.ATIS_ID.split(';')[1];
             }
             address += fromATIS;
             let serviceData = {
-                'category' : 'PR',
+                'category': 'PR',
                 's-location': address
             };
             $.ajax({
-				type: 'get',
+                type: 'get',
                 url: '/Services/FinderSvc.ashx',
-				data: serviceData,
-				dataType: "json"
+                data: serviceData,
+                dataType: "json"
             })
-            .done(function (result, status, xhr) {
-                if (result.error) {
-                    dfd.reject({'Message': result.error});
-                } else {
-                    dfd.resolve(result);
-                }
-            })
-            .fail(function(err) {
-                dfd.reject("ParkRideServiceFinder failed - No results " + err);
-            });
+                .done(function (result, status, xhr) {
+                    if (result.error) {
+                        dfd.reject({ 'Message': result.error });
+                    } else {
+                        dfd.resolve(result);
+                    }
+                })
+                .fail(function (err) {
+                    dfd.reject("ParkRideServiceFinder failed - No results " + err);
+                });
         }).promise();
     }
-    function formatPage (addressChoice) {
+    function formatPage(addressChoice) {
         $('#prFinderResults').empty();
         findNearestParkRides(addressChoice)
-        .then(function(results){
-            //console.dir(results);
-            $('#prFinderResults').append('<p class="result-msg">Nearest Park & Rides to '+addressChoice.attributes.LongLabel+'</p>');
+            .then(function (results) {
+                //console.dir(results);
+                $('#prFinderResults').append('<p class="result-msg">Nearest Park & Rides to ' + addressChoice.attributes.LongLabel + '</p>');
 
-            for (let i=0, l=results.length;i < l; i++) {
-                let stop = results[i];
-                // create map link
-                let ptlatlon = [];
-                CoordinateConversion.UTMXYToLatLon(parseFloat(stop.Y), parseFloat(stop.X), 15, false, ptlatlon);
-                var longitude = CoordinateConversion.RadToDeg(ptlatlon[1]).toFixed(4);
-                var latitude = CoordinateConversion.RadToDeg(ptlatlon[0]).toFixed(4);
+                for (let i = 0, l = results.length; i < l; i++) {
+                    let stop = results[i];
+                    // create map link
+                    let ptlatlon = [];
+                    CoordinateConversion.UTMXYToLatLon(parseFloat(stop.Y), parseFloat(stop.X), 15, false, ptlatlon);
+                    var longitude = CoordinateConversion.RadToDeg(ptlatlon[1]).toFixed(4);
+                    var latitude = CoordinateConversion.RadToDeg(ptlatlon[0]).toFixed(4);
                     let mapLink = '/imap/interactivemap.aspx?x=' + longitude + '&y=' + latitude + '&t=pr';
 
-                $('#prFinderResults').append(`
+                    $('#prFinderResults').append(`
                 <div class="card">
                     <a href="${mapLink}" class="d-flex btn">
                         <p class="d-flex pr-location">${stop.LocationName}&nbsp;(${stop.Distance}&nbsp;mi.)</p>
@@ -85,15 +85,15 @@ var ParkRideServices = (function($,  window, document, undefined) {
                     </a>
                 </div>							
                 `);
-            }
-            if (results.length === 0) {
-                $('#prFinderResults').append('<p class="result-msg">No Park & Rides close to '+addressChoice.address+'</p>');
-            }
-            sessionStorage.setItem('prFinderResults', $('#prFinderResults').html());
-        })
-        .fail(function(err) {
-            $('#prFinderResults').append('<p class="result-msg">No Park & Rides close to '+addressChoice.address+'</p>');
-        });
+                }
+                if (results.length === 0) {
+                    $('#prFinderResults').append('<p class="result-msg">No Park & Rides close to ' + addressChoice.address + '</p>');
+                }
+                sessionStorage.setItem('prFinderResults', $('#prFinderResults').html());
+            })
+            .fail(function (err) {
+                $('#prFinderResults').append('<p class="result-msg">No Park & Rides close to ' + addressChoice.address + '</p>');
+            });
     }
 
     return {
@@ -101,24 +101,24 @@ var ParkRideServices = (function($,  window, document, undefined) {
     };
 })(jQuery, window, document);
 
-$(function() {
+$(function () {
     // This should execute when /park-ride-lots loads, it sets the autocomplete to trigger 
     // the page content when user selects a location to search
     if ($('#parkRides').length) {
         AutocompleteAddress.init("parkRidesSearch",/*UTMout*/ true,
-                function() {
-                    var choice = AutocompleteAddress.getChoice("parkRidesSearch");
-                    ParkRideServices.formatPage(choice);
-                }
-            );
+            function () {
+                var choice = AutocompleteAddress.getChoice("parkRidesSearch");
+                ParkRideServices.formatPage(choice);
+            }
+        );
         if (!(sessionStorage.getItem('prFinderResults') === null)) {
             $('#prFinderResults').html(sessionStorage.getItem('prFinderResults'));
         }
-        $('#prUseCurrentLoc').click(function() {
-            AutocompleteAddress.getUserLocation().then(function(){  // get current location
+        $('#prUseCurrentLoc').click(function () {
+            AutocompleteAddress.getUserLocation().then(function () {  // get current location
                 $('#parkRidesSearch').val('Current Location');
                 let userLoc = AutocompleteAddress.setUserLoc('parkRidesFindMe');  // format the location
-                if (userLoc) { 
+                if (userLoc) {
                     ParkRideServices.formatPage(userLoc); // pass the location to finder service
                 }
             });
