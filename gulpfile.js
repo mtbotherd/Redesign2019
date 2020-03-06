5a74bb8050dff0e24f0f9e99fa5e56de77f8576a
Rich Bothe <rich.bothe@metrotransit.org>
2020-03-06 10:53:43 -0600

    commented out time polyfill due to potential conflict with mement.js.

--- gulpfile.js
+++ gulpfile.js
@@ -29,9 +29,9 @@ gulp.task('vendorjs', function() {
 			'node_modules/popper.js/dist/umd/popper.js.map',
 			'node_modules/bootstrap/dist/js/bootstrap.js',
 			'node_modules/bootstrap/dist/js/bootstrap.js.map',
-			'node_modules/time-input-polyfill/dist/time-input-polyfill.auto.min.js',
-			'node_modules/time-input-polyfill/dist/time-input-polyfill.auto.min.js.map',
-			'node_modules/date-input-polyfill/date-input-polyfill.dist.js',
+			// 'node_modules/time-input-polyfill/dist/time-input-polyfill.auto.min.js',
+			// 'node_modules/time-input-polyfill/dist/time-input-polyfill.auto.min.js.map',
+			// 'node_modules/date-input-polyfill/date-input-polyfill.dist.js',
 			'src/js/vendor/*.js',
 		])
 		.pipe(gulp.dest('dist/js'));
